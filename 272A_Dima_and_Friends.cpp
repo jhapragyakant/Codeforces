a2e42c9382aa0a440c06c9803074b9fe47697781
Kushagra Jha <pkjakv123@gmail.com>
2021-05-06 12:28:41 +0530

    272A_Dima_and_Friends.cpp added

---
+++ 272A_Dima_and_Friends.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    int n;
+    cin >>n;
+    int arr[n];
+    ll sum=0;
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+        sum+=arr[i];
+    }
+    int count =0;
+    for(int i=1;i<=5;i++){
+        if((sum+i)%(n+1)!=1)count++;
+    }
+    cout << count << endl;
+}
